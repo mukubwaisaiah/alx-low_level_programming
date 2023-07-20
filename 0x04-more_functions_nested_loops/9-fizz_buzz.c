9b53d0b83ac7571c07faa40380b05736a3041313
Isaiah Mwiti <isahmwiti@gmail.com>
2023-07-20 02:08:38 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,84 @@
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void fizz_buzz(void);
+
+#endif /* MAIN_H */
+
+/* 9-fizz_buzz.c */
 #include "main.h"
 
 /**
- * more_numbers - prints numbers from 0 to 14, ten times, followed by a new line
+ * fizz_buzz - Prints numbers from 1 to 100, but replaces multiples of 3 with Fizz,
+ * multiples of 5 with Buzz, and multiples of both 3 and 5 with FizzBuzz.
  */
-void more_numbers(void)
+void fizz_buzz(void)
 {
-    int i, num;
+    int i;
 
-    for (i = 0; i < 10; i++)
+    for (i = 1; i <= 100; i++)
     {
-        for (num = 0; num <= 14; num++)
+        if (i % 3 == 0 && i % 5 == 0)
         {
-            if (num > 9)
-                _putchar((num / 10) + '0');
-            _putchar((num % 10) + '0');
+            _putchar('F');
+            _putchar('i');
+            _putchar('z');
+            _putchar('z');
+            _putchar('B');
+            _putchar('u');
+            _putchar('z');
+            _putchar('z');
         }
-        _putchar('\n');
+        else if (i % 3 == 0)
+        {
+            _putchar('F');
+            _putchar('i');
+            _putchar('z');
+            _putchar('z');
+        }
+        else if (i % 5 == 0)
+        {
+            _putchar('B');
+            _putchar('u');
+            _putchar('z');
+            _putchar('z');
+        }
+        else
+        {
+            int num = i;
+            int digits = 0;
+
+            while (num != 0)
+            {
+                num /= 10;
+                digits++;
+            }
+
+            num = i;
+
+            while (digits > 0)
+            {
+                int divisor = 1;
+
+                for (int j = 1; j < digits; j++)
+                {
+                    divisor *= 10;
+                }
+
+                int digit = num / divisor;
+                _putchar('0' + digit);
+                num %= divisor;
+                digits--;
+            }
+        }
+
+        if (i < 100)
+            _putchar(' ');
+
+        if (i == 100)
+            _putchar('\n');
     }
 }
 
