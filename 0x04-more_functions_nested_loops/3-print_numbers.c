ae36350f0a25860ef94db0b68b15c90806e9f007
Isaiah Mwiti <isahmwiti@gmail.com>
2023-07-20 01:44:06 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,26 @@
+/* main.h */
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_numbers(void);
+
+#endif /* MAIN_H */
+
+/* 3-print_numbers.c */
 #include "main.h"
 
 /**
- * mul - multiplies two integers
- * @a: The first integer
- * @b: The second integer
- *
- * Return: The product of a and b
+ * print_numbers - Prints the numbers from 0 to 9, followed by a new line.
  */
-int mul(int a, int b)
+void print_numbers(void)
 {
-    return (a * b);
+    int num;
+
+    for (num = 0; num <= 9; num++)
+    {
+        _putchar(num + '0');
+    }
+    _putchar('\n');
 }
 
