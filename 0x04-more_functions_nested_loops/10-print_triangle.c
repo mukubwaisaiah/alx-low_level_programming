3349c22d29f3ea522c9b48619d8ad5c1e37de1ef
Isaiah Mwiti <isahmwiti@gmail.com>
2023-07-20 00:50:46 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line of underscores, followed by a new line
+ * @n: The number of underscores to print
+ */
+void print_line(int n)
+{
+    int i;
+
+    if (n > 0)
+    {
+        for (i = 0; i < n; i++)
+            _putchar('_');
+    }
+    _put
+
